fe0dca2d12fe2c927a3daf68525941e3b5c83de1
Alexander Neitzel <alexander.neitzel@garmingo.com>
2025-03-22 10:52:31 +0100

    Refactor InlineArrowToFunctionTransformer to simplify transform method and update display name

--- src/transformers/InlineArrowToFunction.ts
+++ src/transformers/InlineArrowToFunction.ts
@@ -1,45 +1,25 @@
-/*
- *   Copyright (c) 2025 Alexander Neitzel
-
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
-
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
-
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 import * as t from "@babel/types";
 import { NodeTransformer, TransformContext } from "./transformers";
 
 export const InlineArrowToFunctionTransformer: NodeTransformer<t.ArrowFunctionExpression> =
   {
     key: "inline-arrow-to-function",
-    displayName: "Inline Arrow to Function Expression",
+    displayName: "Inline Arrow → Function Expression",
     nodeTypes: ["ArrowFunctionExpression"],
     phases: ["pre"],
     test: () => true,
 
-    transform(node, context: TransformContext): t.Node {
+    transform(node): t.FunctionExpression {
       const body = t.isBlockStatement(node.body)
         ? node.body
         : t.blockStatement([t.returnStatement(node.body)]);
 
-      const fn = t.functionExpression(
+      return t.functionExpression(
         null,
         node.params,
         body,
         node.generator ?? false,
         node.async ?? false
       );
-
-      context.path.replaceWith(fn);
-      return fn;
     },
   };

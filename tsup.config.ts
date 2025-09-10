1f0a9d39bd22c53b6c4b26eb763759d26d9d71c2
Alexander Neitzel <alexander.neitzel@garmingo.com>
2025-09-11 00:08:06 +0200

    Bump version to 1.9.9; enhance exports and update entry points in tsup configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,10 +6,17 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: [
+    "src/index.ts",
+    "src/compiler.ts",
+    "src/loader/buffer.ts",
+    "src/loader/file.ts",
+    "src/runtime/default.ts",
+    "src/runtime/safe.ts",
+  ],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
+  splitting: true,
   sourcemap: true,
   clean: true,
 });

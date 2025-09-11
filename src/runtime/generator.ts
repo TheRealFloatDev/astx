aa7e27face68a9e865fafb1575728b24acfb35b6
Alexander Neitzel <alexander.neitzel@garmingo.com>
2025-09-11 21:21:53 +0200

    Bump version to 1.10.2; update generator module import style

--- src/runtime/generator.ts
+++ src/runtime/generator.ts
@@ -15,12 +15,12 @@
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import generate from "@babel/generator";
+import * as generateModule from "@babel/generator";
 import { CompiledProgram } from "..";
 import { decodeToAST } from "../loader/shared";
 
 export function generateJSCode(compiled: CompiledProgram): string {
   const ast = decodeToAST(compiled);
-  const { code } = generate(ast);
+  const { code } = generateModule.default(ast);
   return code;
 }
